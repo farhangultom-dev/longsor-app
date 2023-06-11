7ca123ea94bf2eeabbbcbc2696ca2ea260975346
ibnu <ibnuwildan15@gmail.com>
2023-06-11 17:24:38 +0700

    ver 2 (pop up menu)

--- app/src/main/java/com/example/longsor_app_001/MainActivity.java
+++ app/src/main/java/com/example/longsor_app_001/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.longsor_app_001;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView buttonmenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.buttonmenu = (ImageView)findViewById(R.id.menu_drawer);
+
+        this.buttonmenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, buttonmenu);
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+                popup.show();
+            }
+        });
     }
 }
